c08ce64788631129d4f196074ac3e8347023aeff
emuyia <emuyiahere@gmail.com>
2023-09-09 03:01:35 +0100

    resub with mc embed form & unsub with mc API

--- src/routes/blog.py
+++ src/routes/blog.py
@@ -1,14 +1,17 @@
 from src import app
 from src.models import Post, Tag
 from sqlalchemy import or_
-from flask import render_template, request, flash
+from flask import render_template, request, flash, jsonify
 import requests
+import hashlib
+import json
 
 
 @app.route("/post/<int:post_id>")
 def view_post(post_id):
     post = Post.query.get(post_id)
     return render_template("view_post.html", post=post)
+
 
 '''
 @app.context_processor
@@ -28,15 +31,23 @@ def search_results(query):
 @app.route('/subscribe', methods=['GET', 'POST'])
 def subscribe():
     if request.method == 'POST':
-        email = request.form.get('email')
-        data = {
-            "email_address": email,
-            "status": "subscribed",
-        }
-        response = requests.post(
-            f'https://{app.config["MAILCHIMP_DATACENTER"]}.api.mailchimp.com/3.0/lists/{app.config["MAILCHIMP_LIST_ID"]}/members/',
-            auth=('apikey', app.config["MAILCHIMP_API_KEY"]),
-            json=data
-        )
-        flash('Subscription successful.' if response.status_code == 200 else 'Subscription failed.')
+        data = request.get_json()
+        if data.get('action') == 'Unsubscribe':
+            email_to_unsubscribe = data.get('EMAIL')
+            api_key = app.config["MAILCHIMP_API_KEY"]
+            list_id = app.config["MAILCHIMP_LIST_ID"]
+            server = app.config["MAILCHIMP_DATACENTER"]
+            endpoint = f"https://{server}.api.mailchimp.com/3.0/lists/{list_id}/members/{email_to_unsubscribe}"
+            headers = {
+                'Authorization': f'Bearer {api_key}',
+                'Content-Type': 'application/json'
+            }
+            payload = {'status': 'unsubscribed'}
+            response = requests.patch(endpoint, headers=headers, data=json.dumps(payload))
+            if response.status_code == 200:
+                return jsonify({"message": "Successfully unsubscribed."}), 200
+            else:
+                return jsonify({"message": "Failed to unsubscribe."}), 400
+        else:
+            return jsonify({"message": "Invalid action."}), 400
     return render_template('subscribe.html')
