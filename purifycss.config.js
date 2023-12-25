cd3c94eba4b260ec88a3b3ddea8952c4c68a1ec5
june <emuyiahere@gmail.com>
2023-12-25 02:17:21 +0000

    optimise css

---
+++ purifycss.config.js
@@ -0,0 +1,15 @@
+const purify = require('purify-css');
+
+let content = ['./src/templates/**/*.html', './src/static/js/**/*.js'];
+let css = ['./src/static/css/style.css'];
+
+let options = {
+  output: './src/static/css/purified.css',
+  minify: false,
+  info: true,
+  whitelist: ['dropdown-item', 'badge', 'ckeditor', 'nav-item', 'dropdown', 'dropdown-menu', 'show'] 
+};
+
+purify(content, css, options, function (purifiedResult) {
+  console.log('PurifyCSS has completed.');
+});
