6ac1f61a8a9b9a56b72a60a56ebe8caebc083b31
emuyia <emuyiahere@gmail.com>
2024-03-07 05:34:24 +0000

    quill: add links & images via URL

--- src/static/js/quill_editor.js
+++ src/static/js/quill_editor.js
@@ -18,22 +18,34 @@ document.addEventListener("DOMContentLoaded", function () {
     var quill = new Quill(quillContainer, {
       theme: "snow",
       modules: {
-        toolbar: [
-          ["bold", "italic", "underline", "strike"],
-          ["blockquote", "code-block"],
-          [{ header: 1 }, { header: 2 }],
-          [{ list: "ordered" }, { list: "bullet" }],
-          [{ script: "sub" }, { script: "super" }],
-          [{ indent: "-1" }, { indent: "+1" }],
-          [{ direction: "rtl" }],
-          [{ size: ["small", false, "large", "huge"] }],
-          [{ header: [1, 2, 3, 4, 5, 6, false] }],
-          [{ color: [] }, { background: [] }],
-          [{ font: [] }],
-          [{ align: [] }],
-          ["clean"],
-        ],
-      },
+        toolbar: {
+          container: [
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code-block"],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ script: "sub" }, { script: "super" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            [{ direction: "rtl" }],
+            [{ size: ["small", false, "large", "huge"] }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ color: [] }, { background: [] }],
+            [{ font: [] }],
+            [{ align: [] }],
+            ["clean"],
+            ["link", "image"]
+          ],
+          handlers: {
+            image: function() {
+              var range = this.quill.getSelection();
+              var value = prompt("Enter the image URL:");
+              if (value) {
+                this.quill.insertEmbed(range.index, "image", value, Quill.sources.USER);
+              }
+            }
+          }
+        }
+      }
     });
 
     var aceContainer = document.createElement("div");
