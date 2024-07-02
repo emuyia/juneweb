f0b93981aeefdab264747e3e3cdaa43918f69871
june <emuyiahere@gmail.com>
2024-07-02 23:41:46 +0100

    remove: subscribe & contact pages

--- src/routes/blog.py
+++ src/routes/blog.py
@@ -231,60 +231,6 @@ def search():
     return render_template("search.html", posts=posts, query=query)
 
 
-@app.route("/subscribe", methods=["GET", "POST"])
-def subscribe():
-    api_key = app.config["MAIL_API_KEY"]
-    headers = {
-        "Content-Type": "application/json",
-        "X-MailerLite-ApiKey": api_key,
-    }
-    if request.method == "POST":
-        email = request.form["email"]
-        if "unsubscribe" in request.form:
-            # Unsubscribe Logic
-            response = requests.get(
-                f"https://api.mailerlite.com/api/v2/subscribers/{email}",
-                headers=headers,
-            )
-            if response.status_code == 200:
-                subscriber_id = response.json()["id"]
-                group_id = app.config["MAIL_GROUP_ID"]
-                response = requests.delete(
-                    f"https://api.mailerlite.com/api/v2/groups/{group_id}/subscribers/{subscriber_id}",
-                    headers=headers,
-                )
-                if response.status_code == 200 or response.status_code == 204:
-                    flash("Unsubscribed successfully")
-                    return redirect(url_for("subscribe"))
-                else:
-                    flash("Error unsubscribing")
-                    print(
-                        f"Unsubscribe failed with status {response.status_code}: {response.content}"
-                    )
-            else:
-                flash("Error finding subscriber")
-                print(
-                    f"Subscriber retrieval failed with status {response.status_code}: {response.content}"
-                )
-        elif "subscribe" in request.form:
-            # Subscribe Logic
-            group_id = app.config["MAIL_GROUP_ID"]
-            response = requests.post(
-                f"https://api.mailerlite.com/api/v2/groups/{group_id}/subscribers",
-                headers=headers,
-                json={"email": email},
-            )
-            if response.status_code == 200:
-                flash("Subscribed successfully")
-                return redirect(url_for("subscribe"))
-            else:
-                flash("Error subscribing")
-                print(
-                    f"Subscribe failed with status {response.status_code}: {response.content}"
-                )
-    return render_template("subscribe.html")
-
-
 @app.route("/feed")
 def feed():
     fg = FeedGenerator()
